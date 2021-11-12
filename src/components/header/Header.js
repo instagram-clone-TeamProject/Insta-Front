102f5d768eee521e177ae4a70ff5475ff5a67c6d
dream0214 <52529597+dream0214@users.noreply.github.com>
2021-11-13 01:22:33 +0900

    header update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import instagram_logo from '../../assets/instagram_logo.png'
 
-const Wrapper=styled.div`
+const Wrapper1=styled.div`
   display:flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height:54px;
   margin:0;
@@ -14,11 +16,60 @@ const Wrapper=styled.div`
   z-index:100;
 `;
 
+const Wrapper2=styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 935px;
+  height:54px;
+  margin:0;
+  padding-left:20px;
+  padding-right:20px;
+`;
+
+const InputBox = styled.div`
+    display:flex;
+    justify-content: start;
+    align-items: center;
+    border-radius: 3px;
+    background-color: #FAFAFA;
+    border:1px solid #DBDBDB;
+    width:215px;
+    height:28px;
+`;
+
+const InputConsol = styled.input`
+    background-color: #FAFAFA;
+    border: none;
+    outline: none;
+    width: 400px;
+`;
+
+const IconBox=styled.div`
+  display: flex;
+  justify-content: space-between;
+  width:266px;
+  height:22px;
+`;
+
 function Header() {
   return(
-  <div>
-    <Wrapper />
-  </div>
+  <>
+    <Wrapper1>
+      <Wrapper2>
+      <a href="/">
+        <img src={instagram_logo} alt="logo" height="45px" />
+      </a>
+      <InputBox>
+        <InputConsol placeholder="검색" />
+      </InputBox>
+      <IconBox>
+
+      </IconBox>
+      </Wrapper2>
+    </Wrapper1>
+  </>
   )
 }
 export default Header
